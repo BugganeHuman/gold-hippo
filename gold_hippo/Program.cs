bb518347a1b15f61bf8e406c8c64f4e8cebf73a6
BugganeHuman <markizdelakarabas@gmail.com>
2026-09-23 19:42:29 +0400

    done, wait to review

--- gold_hippo/Program.cs
+++ gold_hippo/Program.cs
@@ -1,1 +1,52 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using Logics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (!File.Exists("balance.txt"))
+        {
+            File.WriteAllText("balance.txt", "0\n\n");
+        }
+        Console.WriteLine("\nHi I'm Gold Hippo your moneybox :)\n");
+        Console.WriteLine("Write a number of action");
+        while (true)
+        {
+            Console.Write("\n1 - put money\n\n2 - get money\n\n3 - show balance\n\n" +
+                "4 - show logs\n\n0 - exit\n            : ");
+            string? choice = Console.ReadLine();
+
+            if (choice == "0")
+            {
+                break;
+            }
+            if (choice == "1" || choice == "2")
+            {
+                Console.Write("\nWrite amount: ");
+                bool is_int = int.TryParse(Console.ReadLine(), out int amount);
+                if (!is_int)
+                {
+                    Console.WriteLine("\nI need a number like 100");
+                    continue;
+                }
+                if (choice == "2")
+                {
+                    amount = -amount;
+                }
+                Logics.Transaction.CreateTransactions(amount);
+                Console.WriteLine("\nDone");
+            }
+            if (choice == "3")
+            {
+                int balance = Logics.Transaction.GetBalance();
+                Console.WriteLine($"\nYou have - {balance}");
+            }
+            if (choice == "4")
+            {
+                Logics.Transaction.ReadTransactions();
+            }
+        }
+
+    } 
+}
---
+++ gold_hippo/Transaction.cs
@@ -0,0 +1,49 @@
+﻿namespace Logics;
+
+using System.IO;
+
+public class Transaction
+{
+    public static void CreateTransactions (int amount)
+    {
+        bool is_balance_int = int.TryParse(File.ReadLines("balance.txt").FirstOrDefault(),
+                                out int balance);
+        if (!is_balance_int)
+        {
+            throw new InvalidOperationException("file balance.txt was broken," +
+                " delete them and restart program");
+        }
+        balance += amount;
+        string log = $"{DateTime.Now}; AMOUNT = {amount}; BALANCE = {balance}\n\n";
+        File.AppendAllText("balance.txt", log ); 
+        string[] fileStrings = File.ReadAllLines("balance.txt");
+        fileStrings[0] = balance.ToString();
+        File.WriteAllLines("balance.txt", fileStrings);
+        
+    }
+
+    public static void ReadTransactions()
+    {
+        string[] fileStrings = File.ReadAllLines("balance.txt");
+        for (int i = 0; i < fileStrings.Length; i++)
+        {
+            if (i != 0)
+            {
+                Console.WriteLine(fileStrings[^i]);
+            }
+        }
+    }
+
+    public static int GetBalance()
+    {
+        bool is_balance_int = int.TryParse(File.ReadLines("balance.txt").FirstOrDefault(),
+                                out int balance);
+        if (!is_balance_int)
+        {
+            throw new InvalidOperationException("file balance.txt was broken," +
+                " delete them and restart program");
+        }
+        return balance;
+    }
+}
+// надо методы - сделать запись типо лог, посмтреть транзации
